3907e8cbde6b3f44dfcae2203b37c956b1098bef
José Yáñez <joseyanez2298@gmail.com>
2025-09-12 18:48:04 -0500

    Inicio del proyecto

---
+++ src/main/java/com/jose/newlibrary/NewlibraryApplication.java
@@ -0,0 +1,13 @@
+package com.jose.newlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewlibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jose/newlibrary/NewlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose.newlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
