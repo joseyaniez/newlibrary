7c5d9c0a3f9291f95b845d3a885e09a87949d2a6
José Yáñez <joseyanez2298@gmail.com>
2025-09-13 18:04:09 -0500

    Generación de clase de implementación

---
+++ src/main/java/com/jose/newlibrary/library/service/impl/NationalityServiceImpl.java
@@ -0,0 +1,41 @@
+
+package com.jose.newlibrary.library.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.jose.newlibrary.library.model.dto.request.NationalityRequest;
+import com.jose.newlibrary.library.model.dto.response.NationalityResponse;
+import com.jose.newlibrary.library.service.NationalityService;
+
+/**
+ * NationalityServiceImpl
+ */
+@Service
+public class NationalityServiceImpl implements NationalityService {
+
+	@Override
+	public NationalityResponse createNationality(NationalityRequest nationalityRequest) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'createNationality'");
+	}
+
+	@Override
+	public NationalityResponse getNationality(Long id) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'getNationality'");
+	}
+
+	@Override
+	public NationalityResponse updateNationality(Long id, NationalityRequest nationalityRequest) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'updateNationality'");
+	}
+
+	@Override
+	public Boolean deleteNationality(Long id) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'deleteNationality'");
+	}
+
+    
+}
