44ced10160609d918288e62969d07dafbb93d85a
José Yáñez <joseyanez2298@gmail.com>
2025-09-13 19:22:39 -0500

    Creación de un controller para el nationality

---
+++ src/main/java/com/jose/newlibrary/library/controller/NationalityController.java
@@ -0,0 +1,16 @@
+
+package com.jose.newlibrary.library.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * NationalityController
+ */
+@RestController
+@RequestMapping("/nationalities")
+public class NationalityController {
+
+
+    
+}
