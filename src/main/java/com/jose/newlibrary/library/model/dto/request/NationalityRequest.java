09ef44ecd2d22c4b019e1a524e864184090db512
José Yáñez <joseyanez2298@gmail.com>
2025-09-14 19:23:08 -0500

    Corrección para números en nombres

--- src/main/java/com/jose/newlibrary/library/model/dto/request/NationalityRequest.java
+++ src/main/java/com/jose/newlibrary/library/model/dto/request/NationalityRequest.java
@@ -5,12 +5,17 @@ package com.jose.newlibrary.library.model.dto.request;
 import com.fasterxml.jackson.annotation.JsonAlias;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 /**
  * NationalityRequest
  */
 public record NationalityRequest(
     @NotBlank
+    @Pattern(
+        regexp = "^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$",
+        message = "El nombre solo puede contener letras y espacios"
+    )
     @JsonAlias({"nationality", "name", "nationality_name"})
     String name
 ) {}
