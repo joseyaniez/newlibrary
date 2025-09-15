45476cc835c2920e6d802c46b0fa5c9a64d2dc18
José Yáñez <joseyanez2298@gmail.com>
2025-09-14 19:14:11 -0500

    Update y Delete en controllers funcional

--- src/main/java/com/jose/newlibrary/library/controller/NationalityController.java
+++ src/main/java/com/jose/newlibrary/library/controller/NationalityController.java
@@ -2,11 +2,15 @@
 package com.jose.newlibrary.library.controller;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +50,20 @@ public class NationalityController {
     public ResponseEntity<NationalityResponse> getNationality(@PathVariable Long id){
         NationalityResponse nationalityResponse = nationalityService.getNationality(id);
         return ResponseEntity.ok(nationalityResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<NationalityResponse> updateNationality(@PathVariable Long id, @Valid @RequestBody NationalityRequest nationalityRequest){
+        NationalityResponse nationalityResponse = nationalityService.updateNationality(id, nationalityRequest);
+        return ResponseEntity.ok(nationalityResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, String>> deleteNationality(@PathVariable Long id){
+        nationalityService.deleteNationality(id);
+        Map<String, String> body = new HashMap<>();
+        body.put("status", "ok");
+        return ResponseEntity.ok(body);
     }
     
 }
