4172dc87300f3f3097a59292a550c7589378813b
José Yáñez <joseyanez2298@gmail.com>
2025-09-20 18:52:37 -0500

    Creación de un AuthenticationManager y UserEntity

---
+++ src/main/java/com/jose/newlibrary/auth/model/entity/UserEntity.java
@@ -0,0 +1,52 @@
+
+package com.jose.newlibrary.auth.model.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * UserEntity
+ */
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    private String varifyOtp;
+    private Boolean isAccountVerified;
+    private Long verifyOtpExpireAt;
+    private String resetOtp;
+    private Long resetOtpExpireAt;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    
+}
