ba5b4bebae6251ea5764584b9325c014c412a193
José Yáñez <joseyanez2298@gmail.com>
2025-09-19 19:47:05 -0500

    Definición de Cors en un Bean aparte

--- src/main/java/com/jose/newlibrary/config/SecurityConfig.java
+++ src/main/java/com/jose/newlibrary/config/SecurityConfig.java
@@ -5,12 +5,18 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * SecurityConfig
@@ -22,14 +28,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors( cors -> cors.configurationSource(request -> {
+            /**.cors( cors -> cors.configurationSource(request -> {
                 var corsConfig = new CorsConfiguration();
                 corsConfig.setAllowedOrigins(List.of("http://localhost:3000"));
                 corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
                 corsConfig.setAllowedHeaders(List.of("*"));
                 corsConfig.setAllowCredentials(true);
                 return corsConfig;
-            }))
+            }))**/
+            .cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.disable());
         return http.build();
     }
@@ -38,5 +45,22 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-    
+
+    @Bean
+    public CorsFilter corsFilter(){
+        return new CorsFilter(corsConfigurationSource());
+    }
+
+    private CorsConfigurationSource corsConfigurationSource(){
+        var corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("http://localhost:3000"));
+        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        corsConfig.setAllowedHeaders(List.of("*"));
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
+    }
+
 }
