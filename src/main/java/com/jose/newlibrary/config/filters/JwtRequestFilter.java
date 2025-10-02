952820e394c064b6955ff79fe79d2c3e1dc8ec8e
José Yáñez <joseyanez2298@gmail.com>
2025-10-02 18:29:44 -0500

    Generación de JwtFilter

--- src/main/java/com/jose/newlibrary/config/filters/JwtRequestFilter.java
+++ src/main/java/com/jose/newlibrary/config/filters/JwtRequestFilter.java
@@ -4,6 +4,10 @@ package com.jose.newlibrary.config.filters;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,6 +16,7 @@ import com.jose.newlibrary.core.util.JwtUtil;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -42,6 +47,33 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
             jwt = authorizationHeader.substring(7);
         }
+
+        // Si el jwt es nulo, buscarlo en las cookies
+        if(jwt == null){
+            Cookie[] cookies = request.getCookies();
+            if(cookies != null){
+                for(Cookie cookie: cookies){
+                    if("jwt".equals(cookie.getName())){
+                        jwt = cookie.getValue();
+                        break;
+                    }
+                }
+            }
+        }
+
+        if(jwt != null){
+            email = jwtUtil.extractEmail(jwt);
+            // el securitycontext debe estar vacío
+            if(email != null && SecurityContextHolder.getContext().getAuthentication() == null){
+                UserDetails userDetails = appUserDetailsService.loadUserByUsername(email);
+                if(jwtUtil.isTokenValid(jwt, userDetails)){
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+        }
+
         filterChain.doFilter(request, response);
     }
 }
